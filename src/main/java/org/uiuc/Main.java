a892d34ddf8844a40612603a9a4d89fb833d56ec
akshathsk <akshath.sk@gmail.com>
2022-10-03 18:23:06 -0500

    Second commit

--- src/main/java/org/uiuc/Main.java
+++ src/main/java/org/uiuc/Main.java
@@ -5,9 +5,14 @@ public class Main {
     int a = 10;
     int b = 20;
     System.out.println("Sum = " + add(a, b));
+    System.out.println("Difference = " + subtract(a, b));
   }
 
   public static int add(int x, int y) {
     return x + y;
+  }
+
+  public static int subtract(int x, int y) {
+    return x - y;
   }
 }
